7d0f9bec7887e46e9f4f4c6a34bef2e0f3b49f5f
bl4ck_russ1an <102438376+NVainer@users.noreply.github.com>
2025-08-23 15:03:02 +0300

    Update server_GUI.sh

--- fast_server/server_GUI.sh
+++ fast_server/server_GUI.sh
@@ -2,8 +2,15 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y xfce4 xfce4-goodies xfce4-session
 sudo apt purge -y lightdm # to prevent this shit from conflict startup
 sudo DEBIAN_FRONTEND=noninteractive apt install -y gdm3
+
 # Set default session for GDM3 (match the file name in /usr/share/xsessions/)
 sudo sed -i 's/^#\?DefaultSession=.*/DefaultSession=xfce.desktop/' /etc/gdm3/custom.conf || \
 echo -e "[daemon]\nDefaultSession=xfce.desktop" | sudo tee -a /etc/gdm3/custom.conf
+
+# always start XFCE
+echo -e "[User]\nXSession=xfce" | sudo tee /var/lib/AccountsService/users/$USER
+sudo chmod 644 /var/lib/AccountsService/users/$USER
+echo "xfce4-session" > ~/.xsession && chmod +x ~/.xsession
+
 sudo systemctl enable gdm3
 sudo reboot
