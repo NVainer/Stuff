aeaf23622aa515da171077355942a7a5d472ec89
bl4ck_russ1an <102438376+NVainer@users.noreply.github.com>
2025-10-01 23:34:04 +0300

    Create fast.sh

---
+++ fast_kali/fast.sh
@@ -0,0 +1,9 @@
+
+
+sudo apt update && sudo apt install -y \
+nmap wireshark aircrack-ng hashcat hydra gobuster sqlmap \
+john netcat-traditional tcpdump metasploit-framework \
+openvpn whois nikto
+
+# kali tools...
+sudo apt install kali-tools-top10
