ad8a4e85f37752d7a6469564b10520f065e73eff
bl4ck_russ1an <102438376+NVainer@users.noreply.github.com>
2025-10-02 01:04:36 +0300

    Update fast.sh

--- fast_kali/fast.sh
+++ fast_kali/fast.sh
@@ -2,8 +2,18 @@
 
 sudo apt update && sudo apt install -y \
 nmap wireshark aircrack-ng hashcat hydra gobuster sqlmap \
-john netcat-traditional tcpdump metasploit-framework \
-openvpn whois nikto
+john netcat-traditional tcpdump \
+openvpn whois nikto \
+postgresql postgresql-contrib libpq-dev
+sudo systemctl enable --now postgresql
+
+# essentials for metasploit
+sudo apt install -y build-essential zlib1g zlib1g-dev libpq-dev libpcap-dev libsqlite3-dev ruby ruby-dev
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+chmod +x msfinstall
+sudo ./msfinstall
+msfdb init
+
 
 # kali tools...
-sudo apt install kali-tools-top10
+
